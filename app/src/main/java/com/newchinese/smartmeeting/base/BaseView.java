37dfb9aff09f83c44e1a9d62ef384fc18b09df8e
xulei <xulei964270141@qq.com>
2017-08-17 15:54:40 +0800

    封包

---
+++ app/src/main/java/com/newchinese/smartmeeting/base/BaseView.java
@@ -0,0 +1,11 @@
+package com.newchinese.smartmeeting.base;
+
+/**
+ * Description:   MVP中的基本view接口类
+ * author         xulei
+ * Date           2017/8/17
+ */
+
+public interface BaseView {
+    
+}
---
+++ app/src/main/java/com/newchinese/smartmeeting/ui/main/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.newchinese.smartmeeting.ui.main.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.newchinese.smartmeeting.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
