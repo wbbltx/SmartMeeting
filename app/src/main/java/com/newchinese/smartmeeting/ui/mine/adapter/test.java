fe8d26cb5417fdc69cf210eba32e60cd3291429d
xulei <xulei964270141@qq.com>
2017-08-18 12:18:19 +0800

    基本类修改

---
+++ app/src/main/java/com/newchinese/smartmeeting/ui/mine/adapter/test.java
@@ -0,0 +1,10 @@
+package com.newchinese.smartmeeting.ui.mine.adapter;
+
+/**
+ * Description:
+ * author         xulei
+ * Date           2017/8/18
+ */
+
+public class test {
+}
---
+++ app/src/main/java/com/newchinese/smartmeeting/widget/test.java
@@ -0,0 +1,10 @@
+package com.newchinese.smartmeeting.widget;
+
+/**
+ * Description:
+ * author         xulei
+ * Date           2017/8/18
+ */
+
+public class test {
+}
