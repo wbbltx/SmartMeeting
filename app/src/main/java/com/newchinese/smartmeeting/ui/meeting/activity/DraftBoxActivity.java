19a86df5c13c2c38c4969cdd5e716f50d9a1657a
xulei <xulei964270141@qq.com>
2017-08-19 11:29:21 +0800

    数据库框架

--- app/src/main/java/com/newchinese/smartmeeting/ui/meeting/activity/DraftBoxActivity.java
+++ app/src/main/java/com/newchinese/smartmeeting/ui/meeting/activity/DraftBoxActivity.java
@@ -2,18 +2,31 @@ package com.newchinese.smartmeeting.ui.meeting.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.newchinese.smartmeeting.R;
 import com.newchinese.smartmeeting.base.BaseActivity;
 import com.newchinese.smartmeeting.base.BasePresenter;
 import com.newchinese.smartmeeting.presenter.DraftBoxPresenter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /**
- * Description:   
+ * Description:
  * author         xulei
  * Date           2017/8/18 16:34
  */
 public class DraftBoxActivity extends BaseActivity {
+    @BindView(R.id.iv_back)
+    ImageView ivBack; //返回
+    @BindView(R.id.tv_title)
+    TextView tvTitle; //标题
+    @BindView(R.id.iv_pen)
+    ImageView ivPen; //笔图标
     private String classifyName; //分类名
 
     @Override
@@ -41,5 +54,15 @@ public class DraftBoxActivity extends BaseActivity {
     @Override
     protected BasePresenter initPresenter() {
         return new DraftBoxPresenter();
+    }
+
+    @OnClick({R.id.iv_back, R.id.iv_pen})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.iv_back:
+                break;
+            case R.id.iv_pen:
+                break;
+        }
     }
 }
