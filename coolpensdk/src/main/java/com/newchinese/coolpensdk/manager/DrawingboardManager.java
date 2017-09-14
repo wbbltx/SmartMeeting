5dedf70616d67e02a17cf0e9e2a01bbf89cc5aef
xulei <xulei964270141@qq.com>
2017-09-14 10:51:58 +0800

    服务条款

--- coolpensdk/src/main/java/com/newchinese/coolpensdk/manager/DrawingboardManager.java
+++ coolpensdk/src/main/java/com/newchinese/coolpensdk/manager/DrawingboardManager.java
@@ -1,15 +1,8 @@
 package com.newchinese.coolpensdk.manager;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
-import android.os.Build;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -26,10 +19,8 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.NetworkInterface;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
--- coolpensdk/src/main/java/com/newchinese/coolpensdk/utils/GetAddressUtil.java
+++ coolpensdk/src/main/java/com/newchinese/coolpensdk/utils/GetAddressUtil.java
@@ -35,72 +35,73 @@ public class GetAddressUtil {
             szImei = TelephonyMgr.getDeviceId();
         } else {
             szImei = getAndroidIdAddress(context);
+            if (szImei == null) szImei = "";
             Log.e("coolPenError", "11008:无READ_PHONE_STATE权限");
         }
         return szImei;
     }
 
-    /**
-     * 获取设备Mac地址
-     */
-    public static String getMacAddress(Context context) {
-        Log.e("test_address", "IMEIAddress:" + getIMEIAddress(context));
-        Log.e("test_address", "BlueAddress:" + getBlueAddress());
-        Log.e("test_address", "AndroidIdAddress:" + getAndroidIdAddress(context));
+//    /**
+//     * 获取设备Mac地址
+//     */
+//    public static String getMacAddress(Context context) {
+//        Log.e("test_address", "IMEIAddress:" + getIMEIAddress(context));
+//        Log.e("test_address", "BlueAddress:" + getBlueAddress());
+//        Log.e("test_address", "AndroidIdAddress:" + getAndroidIdAddress(context));
+//
+//        String macAddress = null;
+//        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+//        WifiInfo info = (null == wifiManager ? null : wifiManager.getConnectionInfo());
+//        if (!wifiManager.isWifiEnabled()) {
+//            //必须先打开，才能获取到MAC地址
+//            wifiManager.setWifiEnabled(true);
+//            wifiManager.setWifiEnabled(false);
+//        }
+//        if (null != info) {
+//            macAddress = info.getMacAddress();
+//        }
+//        if ("02:00:00:00:00:00".equals(macAddress)) {
+//            return get6MacAddress();
+//        }
+//        return macAddress;
+//    }
+//
+//    /**
+//     * 获取6.0及以上设备Mac地址
+//     */
+//    private static String get6MacAddress() {
+//        try {
+//            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+//            for (NetworkInterface nif : all) {
+//                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+//                byte[] macBytes = nif.getHardwareAddress();
+//                if (macBytes == null) {
+//                    return "";
+//                }
+//                StringBuilder res1 = new StringBuilder();
+//                for (byte b : macBytes) {
+//                    res1.append(String.format("%02X:", b));
+//                }
+//                if (res1.length() > 0) {
+//                    res1.deleteCharAt(res1.length() - 1);
+//                }
+//                return res1.toString();
+//            }
+//        } catch (Exception e) {
+//            Log.e("coolPenError", "11008:获取Mac地址失败");
+//        }
+//        return "02:00:00:00:00:00";
+//    }
 
-        String macAddress = null;
-        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-        WifiInfo info = (null == wifiManager ? null : wifiManager.getConnectionInfo());
-        if (!wifiManager.isWifiEnabled()) {
-            //必须先打开，才能获取到MAC地址
-            wifiManager.setWifiEnabled(true);
-            wifiManager.setWifiEnabled(false);
-        }
-        if (null != info) {
-            macAddress = info.getMacAddress();
-        }
-        if ("02:00:00:00:00:00".equals(macAddress)) {
-            return get6MacAddress();
-        }
-        return macAddress;
-    }
-
-    /**
-     * 获取6.0及以上设备Mac地址
-     */
-    private static String get6MacAddress() {
-        try {
-            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
-            for (NetworkInterface nif : all) {
-                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
-                byte[] macBytes = nif.getHardwareAddress();
-                if (macBytes == null) {
-                    return "";
-                }
-                StringBuilder res1 = new StringBuilder();
-                for (byte b : macBytes) {
-                    res1.append(String.format("%02X:", b));
-                }
-                if (res1.length() > 0) {
-                    res1.deleteCharAt(res1.length() - 1);
-                }
-                return res1.toString();
-            }
-        } catch (Exception e) {
-            Log.e("coolPenError", "11008:获取Mac地址失败");
-        }
-        return "02:00:00:00:00:00";
-    }
-
-    /**
-     * 获取蓝牙设备地址
-     */
-    public static String getBlueAddress() {
-        BluetoothAdapter m_BluetoothAdapter = null; // Local Bluetooth adapter      
-        m_BluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        String m_szBTMAC = m_BluetoothAdapter.getAddress();
-        return m_szBTMAC;
-    }
+//    /**
+//     * 获取蓝牙设备地址
+//     */
+//    public static String getBlueAddress() {
+//        BluetoothAdapter m_BluetoothAdapter = null; // Local Bluetooth adapter      
+//        m_BluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+//        String m_szBTMAC = m_BluetoothAdapter.getAddress();
+//        return m_szBTMAC;
+//    }
 
     /**
      * 获取Android ID
